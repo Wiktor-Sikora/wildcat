bf5d6c8f15eb195b91ae05174e460005fa753a00
Dawid Grys <grysdawid366@gmail.com>
2023-02-09 13:35:32 +0100

    09.02.2024 wypelniacz

--- wildcat/infinite_scroll/urls.py
+++ wildcat/infinite_scroll/urls.py
@@ -7,4 +7,6 @@ from . import views
 urlpatterns = [
     path('test', views.test, name='test'),
     path('more', views.more, name='more'),
+    path('create', views.create, name='create'),
+    path('delete', views.delete, name='delete'),
 ]
--- wildcat/infinite_scroll/views.py
+++ wildcat/infinite_scroll/views.py
@@ -26,3 +26,16 @@ def more(request):
     """ This is the view that dynamically loads more content """
     list_items = Product.objects.all()
     return more_items(request, list_items, 'infinite_scroll/more.html', forever=FOREVER)
+
+def create(request):
+    if request.method == 'POST':
+        number= int(request.POST["number"])
+        for i in range(1, number):
+            product = Product(name=i, description=i, price=i)
+            product.save() 
+    return render(request, 'wypelniacz.html')
+def delete(request):
+    if request.method == 'POST':
+            product = Product.objects.all()
+            product.delete() 
+    return render(request, 'wypelniacz.html')
