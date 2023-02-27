b520c9534ce04db941451a0821624ead336ec837
Wiktor-Sikora <sikora.wiktor777@gmail.com>
2023-02-28 00:31:37 +0100

    country, city field and settings

--- wildcat/accounts/models.py
+++ wildcat/accounts/models.py
@@ -14,6 +14,8 @@ class User(AbstractUser):
     modified = models.DateTimeField(null=True)
     follows = models.ManyToManyField('self', symmetrical=False)
     bio = models.TextField(default=f"Hi, I'm beginning inventor", blank=True, null=True)
+    country = models.CharField(max_length=30, blank=True, null=True)
+    city = models.CharField(max_length=30, blank=True, null=True)
     image = models.ImageField(upload_to='accounts/', default='accounts/default.jpg')
     first_name = None
     last_name = None
