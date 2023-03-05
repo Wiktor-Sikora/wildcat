1586c8e318c45f1ce6d0c31ba872340b87d5d71a
Wiktor-Sikora <sikora.wiktor777@gmail.com>
2023-03-05 20:02:36 +0100

    better ai completion

--- wildcat/common/openai/openai_completion.py
+++ wildcat/common/openai/openai_completion.py
@@ -1,17 +1,23 @@
-import openai, os, json
-import os
+import openai
+import os 
 import json
 from dotenv import load_dotenv
 
 def open_ai_completion(description):
-    openai.api_key = os.getenv("OPENAI_KEY", None)
-    response = openai.Completion.create(
-        engine = 'text-davinci-003',
-        prompt = '(Return json, be cautious abount "needs", example:{"tags":["tags"],"needs":["things"})' + description,
-        stop='.',
-        max_tokens = 200,
-        temperature=0.5
+    openai.api_key = os.getenv('OPENAI_KEY', None)
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {'role': 'system', 'content': 'You are a helpful assistant that returns json {"tags": [...], "missing_parts": [...]}'},
+            {'role': 'user', 'content': 'it\'s a watch, it shows what time it is. This watch is missing a band'},
+            {'role': 'assistant', 'content': '{"tags": ["watch", "clock", "time", "timer", "ticker"], "missing_parts": ["band", "armband", "armlet"]}'},
+            {'role': 'user', 'content': description}
+        ],
+        max_tokens=200,
+        temperature=0.2
     )
-    tags = json.loads(response["choices"][0]["text"]).get("tags", [])
-    needs = json.loads(response["choices"][0]["text"]).get("needs", [])
+
+    tags = json.loads(response['choices'][0]['message']['content']).get('tags', [])
+    needs = json.loads(response['choices'][0]['message']['content']).get('missing_parts', [])
+    
     return tags, needs
